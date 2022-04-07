8538391361d60bc6b543d59c3e4f1d932b5bb002
Patrick Mennen <patrick.mennen@philips.com>
2022-04-07 08:45:00 +0200

    write package.json in generator

--- remix.init/index.js
+++ remix.init/index.js
@@ -1,18 +1,19 @@
-const figlet = require('figlet');
+const fs = require('fs/promises');
+const path = require('path');
+const sort = require('sort-package-json');
 
-const main = ({ rootDirectory }) => {
-  figlet('Remix Demo', (err, data) => {
-    if (err) {
-      return;
-    }
+const main = async ({ rootDirectory }) => {
+  const README = path.join(rootDirectory, 'README.md');
+  const PACKAGE_JSON = path.join(rootDirectory, 'package.json');
 
-    console.log(
-      `${data}
+  const DIR_NAME = path.basename(rootDirectory);
+  const APP_NAME = `${DIR_NAME}`.replace(/[^a-zA-Z0-9-_]/g, '-');
 
-    Bootstrapped in: ${rootDirectory}
-    `.trim(),
-    );
-  });
+  const packageJSON = await fs.readFile(PACKAGE_JSON, 'utf-8');
+  const readmeFile = await fs.readFile(README, 'utf-8');
+
+  const newPackageJSON = JSON.stringify(sort({ ...packageJSON, name: APP_NAME }));
+  await fs.writeFile(PACKAGE_JSON, newPackageJSON);
 };
 
 module.exports = main;
