b96ac80fb8022072e88e7037d678d11794b9f570
Slave Mitev <slave.mitev@philips.com>
2022-04-11 13:56:45 +0200

    layout and login components

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,8 +1,19 @@
-import { login } from '~/api/login.server';
-import { Form, useActionData, useLoaderData } from '@remix-run/react';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import {
+  Alert,
+  Avatar,
+  Box,
+  Button,
+  Container, Grid,
+  Link,
+  TextField,
+  Typography
+} from '@mui/material';
 import { ActionFunction, json, LoaderFunction, redirect } from '@remix-run/node';
-import { commitSession, getSession } from '~/sessions';
+import { Form, useActionData, useLoaderData } from '@remix-run/react';
 import { z } from 'zod';
+import { login } from '~/api/login.server';
+import { commitSession, getSession } from '~/sessions';
 
 const formData = z.object({
   username: z.string(),
@@ -45,7 +56,7 @@ export const loader: LoaderFunction = async ({ request }) => {
   const session = await getSession(request.headers.get('Cookie'));
 
   return {
-    userId: session.get('userId') ?? 'Not logged in.',
+    userId: session.get('userId'),
   };
 };
 
@@ -55,21 +66,58 @@ export default function LoginPage() {
 
   return (
     <Form method="post">
-      <p>Currently logged in as: {userId}</p>
-
-      <label>
-        Username:
-        <input type={'text'} name={'username'} />
-      </label>
-
-      <label>
-        Password:
-        <input type={'password'} name={'password'} />
-      </label>
-
-      {loginAction && loginAction.loginFailed && <p>Your username or password is incorrect.</p>}
-
-      <button type="submit">Login</button>
+      <Container component="main" maxWidth="xs">
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            {userId ? `Currently logged in as: ${userId}` : 'Sign in'}
+          </Typography>
+          <Box>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="username"
+              label="Username"
+              name="username"
+              autoComplete="username"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            {loginAction && loginAction.loginFailed && (
+              <Alert severity="error">Your username or password is incorrect.</Alert>
+            )}
+            <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item>
+                <Link href="#" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
     </Form>
   );
 }
